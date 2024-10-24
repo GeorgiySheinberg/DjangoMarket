df776daa4d03121df4fbae5614f16c18d221c5c0
Georgiy Sheynberg <georgiysheinberg@gmail.com>
2024-10-24 19:50:34 +0300

    models + add PostgreSQL

--- market/market/settings.py
+++ market/market/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'market_db',
+        'USER': 'georgiy',
+        'PASSWORD': 'jocker12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
